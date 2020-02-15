04aae778d720a5600edf93e0513d6f5224b9cbba
Priya Talreja <priyatalreja@Priyas-MacBook-Air.local>
2020-02-15 13:18:59 -0800

    added graphs for hot flashes and sleep data using charts js

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,87 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+
+  hotflashchart: any;
+  sleepdatachart: any;
+
 
   constructor() {}
+
+  ngOnInit() {
+    const dataPoints = [{
+      x: 1,
+      y: 10
+    }, {
+      x: 2,
+      y: 20
+    },
+    {
+      x: 3,
+      y: 0
+    }];
+    const labels = ['a','b', 'c'];
+
+    this.hotflashchart = new Chart('hotflashes', {
+      type: 'bar',
+      data: {
+        labels: labels, // your labels array
+        datasets: [
+          {
+            data: dataPoints, // your data array
+            borderColor: '#00AEFF',
+            fill: false
+          }
+        ]
+      },
+      options: {
+        legend: {
+          display: false
+        },
+        scales: {
+          xAxes: [{
+            display: true
+          }],
+          yAxes: [{
+            display: true
+          }],
+        }
+      }
+    });
+
+    this.sleepdatachart = new Chart('sleepdata', {
+      type: 'line',
+      data: {
+        labels: labels, // your labels array
+        datasets: [
+          {
+            data: dataPoints, // your data array
+            borderColor: '#00AEFF',
+            fill: false
+          }
+        ]
+      },
+      options: {
+        legend: {
+          display: false
+        },
+        scales: {
+          xAxes: [{
+            display: true
+          }],
+          yAxes: [{
+            display: true
+          }],
+        }
+      }
+    });
+  }
 
 }
